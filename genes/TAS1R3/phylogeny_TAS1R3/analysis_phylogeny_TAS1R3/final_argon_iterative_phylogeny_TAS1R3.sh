f0f838082f644c9cc92fc5a3b022f110f9f46500
mackcross <mackenzie-cross@uiowa.edu>
2023-06-30 16:58:47 -0500

    updating sh script for TAS1R3

--- genes/TAS1R3/phylogeny_TAS1R3/analysis_phylogeny_TAS1R3/final_argon_iterative_phylogeny_TAS1R3.sh
+++ genes/TAS1R3/phylogeny_TAS1R3/analysis_phylogeny_TAS1R3/final_argon_iterative_phylogeny_TAS1R3.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-#$ -N name
-#$ -e /Users/mackcross/example.err
-#$ -o /Users/mackcross/example.out
+#$ -N paup_TAS1R3
+#$ -e /Users/mackcross/paup_TAS1R3.err
+#$ -o /Users/mackcross/paup_TAS1R3.out
 #$ -M mackenzie-cross@uiowa.edu
 #$ -m abe
 
 cd /Users/mackcross/
 
-module load stack/2021.1
+module load stack/2022.1
+module load r/4.1.3_gcc-9.4.0
 
 Rscript final_argon_iterative_phylogeny_TAS1R3.R
